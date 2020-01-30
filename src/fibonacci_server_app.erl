ac564910bfff0f275c5126282b32cbc122255f30
root <root@serenova.valuelabs.com>
2020-01-30 09:09:23 +0000

    sysylog testing

--- src/fibonacci_server_app.erl
+++ src/fibonacci_server_app.erl
@@ -15,24 +15,7 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    Routes = [ {
-        '_',
-        [
-            {"/fibonacci/compute", compute_handler, []},
-            {"/fibonacci/history_count", count_handler, []},
-            {"/fibonacci/history", history_handler, []}
-        ]
-    } ],
-    Dispatch = cowboy_router:compile(Routes),
-    NumAcceptors = 10,
-    TransOpts = [ {ip, {0,0,0,0}}, {port, 5001} ],
-    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
-    {ok,_}  =compute_pool:start(),
-    {ok, Pid} = cowboy:start_http(fibonacci_server,
-        NumAcceptors, TransOpts, ProtoOpts),
-
-    {ok,Pid}.
-
+   { ok,self()}.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
